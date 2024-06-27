e2fc80f90df840efdb52334c4dc0a9ae147d0a3f
diegomerlo <diego.e.merlo@gmail.com>
2024-06-27 17:11:32 -0300

    Update

--- first.js
+++ first.js
@@ -1,36 +1,20 @@
 
-alert("Bienvenido :)")
-let nombre = prompt("¿Cuál es tu nombre?")
+alert("Bienvenido :)");
 
-alert("Bienvenido "+{nombre})
+let nombre = prompt("¿Cuál es tu nombre?");
+alert("Bienvenido " + nombre);
 
-// var num = 2;
-// console.log(num);
+var edad;
+let edadCorrecta = false;
 
-// var num2 = 3;
+while (!edadCorrecta) {
+    edad = parseInt(prompt("¿Cuál es tu edad?"));
 
-// console.log(num2);
+    if (isNaN(edad) || edad>99 || edad < 1) {
+        alert("Ingrese un número válido para la edad.");
+        edadCorrecta = false;
+    } else {
+        edadCorrecta = true;
+    }
+}
 
-// var sumaNumeros = num + num2;
-
-// console.log(sumaNumeros);
-
-// var edad;
-
-// let edadCorrecta = false;
-
-// while(!edadCorrecta){
-    
-//     edad = prompt("¿Cuál es tu edad?");
-    
-//     // Intenta convertir la entrada a un número entero
-//     let edadNumerica = parseInt(edad);
-
-//     if (isNaN(edadNumerica)) {
-//         alert("Por favor, ingrese un número válido para la edad.");
-//         edadCorrecta = false;
-//     } else {
-//         edadCorrecta = true;
-//         edad = edadNumerica; // Asigna el valor convertido a edad
-//     }
-// }
