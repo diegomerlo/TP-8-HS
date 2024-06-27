79bac50b984b6738e28e1afc568022dfba31dc92
diegomerlo <diego.e.merlo@gmail.com>
2024-06-27 17:05:48 -0300

    update

--- first.js
+++ first.js
@@ -1,18 +1,32 @@
 
 alert("Bienvenido :)")
-
 let nombre = prompt("¿Cuál es tu nombre?")
 
 alert("Bienvenido "+{nombre})
 
-var num = 2;
+// var num = 2;
+// console.log(num);
 
-console.log(num);
+// var num2 = 3;
 
-var num2 = 3;
+// console.log(num2);
 
-console.log(num2);
+// var sumaNumeros = num + num2;
 
-var sumaNumeros = num + num2;
+// console.log(sumaNumeros);
 
-console.log(sumaNumeros);
+var edad;
+
+let edadCorrecta = false;
+
+while(!edadCorrecta){
+    
+    edad = parseInt(prompt("¿Cual es tu edad?"));
+
+    if(isNaN(edad)){
+        alert("Ingrese un numero para la edad")
+        edadCorrecta = false;
+    } else {
+        edadCorrecta = true;
+    }
+}
