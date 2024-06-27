9293cf78d4b3ddbef7f6109f5324363390e62c00
diegomerlo <diego.e.merlo@gmail.com>
2024-06-27 18:12:04 -0300

    update

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,17 +1,15 @@
 // Declaración de variables
-let nombre = "Juan";
+let nombre = "Leon";
 let edad = 30;
-let cumpleaños = "01 de enero";
+let cumpleaños = "15 de enero";
 let ciudad = "Buenos Aires";
-let ocupacion = "Ingeniero";
+let ocupacion = "Programador";
 let pasatiempos = "leer y jugar al fútbol";
 
 // Escribir un párrafo combinando las variables
 console.log(`Hola, soy ${nombre}, tengo ${edad} años y nací el ${cumpleaños}. Vivo en ${ciudad} y soy ${ocupacion}. En mi tiempo libre me gusta ${pasatiempos}.`);
 
 // Declaración múltiple en una sola línea
-let direccion = "Calle Principal";
-let codigoPostal = "12345";
-let telefono = "123-456-7890";
+let direccion = "Calle Principal", codigoPostal = "12345", telefono = "123-456-7890";
 
 console.log(`Mi dirección es ${direccion}, mi código postal es ${codigoPostal} y mi número de teléfono es ${telefono}.`);
---
+++ ejercicio3.js
@@ -0,0 +1,5 @@
+let edad = prompt("Ingrese su edad: ");
+
+edadDias = edad*365;
+
+alert("Llevas en este mundo "+edadDias);
