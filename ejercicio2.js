3126114a19c866c1af647206304c7c98d6a6c161
diegomerlo <diego.e.merlo@gmail.com>
2024-06-27 17:15:24 -0300

    Update

---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+// Declaración de variables
+let nombre = "Juan";
+let edad = 30;
+let cumpleaños = "01 de enero";
+let ciudad = "Buenos Aires";
+let ocupacion = "Ingeniero";
+let pasatiempos = "leer y jugar al fútbol";
+
+// Escribir un párrafo combinando las variables
+console.log(`Hola, soy ${nombre}, tengo ${edad} años y nací el ${cumpleaños}. Vivo en ${ciudad} y soy ${ocupacion}. En mi tiempo libre me gusta ${pasatiempos}.`);
+
+// Declaración múltiple en una sola línea
+let direccion = "Calle Principal";
+let codigoPostal = "12345";
+let telefono = "123-456-7890";
+
+console.log(`Mi dirección es ${direccion}, mi código postal es ${codigoPostal} y mi número de teléfono es ${telefono}.`);
